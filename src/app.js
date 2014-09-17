414742e8ea3f8f1ef4a6cfa2eade60daeb378da1
SamCook42 <gitsam@samlr.com>
2014-09-17 23:08:19 +0100

    add ability to add and remove boxes

--- src/app.js
+++ src/app.js
@@ -8,30 +8,50 @@
     var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 
     // create Oscillator node
-    var oscillator = audioCtx.createOscillator();
-    var gainNode = audioCtx.createGain();
-    var initialGain = 0.05;
-    var WIDTH = window.innerWidth;
-    var HEIGHT = window.innerHeight;
+    var tones = [];
+    var masterGainNode = audioCtx.createGain();
+    var BOX_SIZE = 50;
+    var MAX_GAIN = 0.02;
+    var MIN_FREQUENCY = 2000;
+    var MAX_FREQUENCY = 5000;
+    var WIDTH = 800;
+    var HEIGHT = 600;
 
-    var CurX, CurY;
+    var freqScaler = (MAX_FREQUENCY - MIN_FREQUENCY)/WIDTH;
+    var gainScaler = 1.0/HEIGHT;
 
-    oscillator.connect(gainNode);
-    
-    gainNode.gain.value = initialGain;
+    masterGainNode.gain.value = MAX_GAIN;
 
-    oscillator.type = 'square';
-    oscillator.frequency.value = 3000; // value in hertz
-    oscillator.start();
+    function addTone (point) {
+        var oscillatorNode = audioCtx.createOscillator();
+        var gainNode = audioCtx.createGain();
+
+        gainNode.gain.value = point.y * gainScaler;
+
+        oscillatorNode.type = 'sine';
+        oscillatorNode.frequency.value = point.x * freqScaler;
+
+        oscillatorNode.connect(gainNode);
+        gainNode.connect(masterGainNode);
+
+        oscillatorNode.start();
+
+        // Return a clean up function
+        return function () {
+            oscillatorNode.stop();
+            gainNode.disconnect(masterGainNode);
+            oscillatorNode.disconnect(gainNode);
+        };
+    }
 
     var mute = document.querySelector('#mute');
     mute.onclick = function() {
         if (mute.getAttribute('data-muted') === "false") {
-            gainNode.disconnect(audioCtx.destination);
+            masterGainNode.disconnect(audioCtx.destination);
             mute.setAttribute('data-muted', 'true');
             mute.innerHTML = "unmute";
         } else {
-            gainNode.connect(audioCtx.destination);
+            masterGainNode.connect(audioCtx.destination);
             mute.setAttribute('data-muted', 'false');
             mute.innerHTML = "mute";
         }
@@ -44,16 +64,60 @@
     var canvasCtx = canvas.getContext('2d');
 
     canvas.onclick = updatePage;
+    canvasCtx.fillStyle = "#FFFFFF";
+    canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+    canvasCtx.strokeRect(1, 1, WIDTH-1, HEIGHT-1);
 
     function updatePage (e) {
         var bounding = canvas.getBoundingClientRect();
-        CurX = e.clientX - canvas.offsetLeft;
-        CurY = e.clientY - canvas.offsetTop;
+        var toRemove = null;
+        var point = {
+            x : e.clientX - canvas.offsetLeft,
+            y : e.clientY - canvas.offsetTop
+        };
+        var box = {
+            x1: point.x - BOX_SIZE/2,
+            y1: point.y - BOX_SIZE/2,
+            x2: point.x + BOX_SIZE/2,
+            y2: point.y + BOX_SIZE/2
+        };
+        var tt, i;
 
-        console.log(CurX, CurY);
+        for (i = tones.length - 1; i >= 0; i--) {
+            tt = tones[i];
+            if (isInBox(point, tt.box)) {
+                tt.removeBox();
+                toRemove = i;
+                break;
+            }
+        }
 
+        if (toRemove !== null) {
+            tones.splice(toRemove, 1);
+        } else {
+            tones.push({
+                box:       box,
+                removeBox: addTone(point)
+            });
+        }
+
+        canvasCtx.fillStyle = "#FFFFFF";
+        canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+        canvasCtx.strokeRect(1, 1, WIDTH-1, HEIGHT-1);
         canvasCtx.fillStyle = "#FF0000";
-        canvasCtx.fillRect(CurX-25, CurY-25, 50, 50);
+        for (i = tones.length - 1; i >= 0; i--) {
+            tt = tones[i];
+            canvasCtx.fillRect(tt.box.x1, tt.box.y1, BOX_SIZE, BOX_SIZE);
+        }
+    }
+
+    function isInBox(point, box) {
+        if (box.x1 < point.x && point.x < box.x2) {
+            if (box.y1 < point.y && point.y < box.y2) {
+                return true;
+            }
+        } 
+        return false;
     }
 
 }());
