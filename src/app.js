3d299d638390f1c1b30f27bc90b4b8f759e8fe3f
SamCook42 <gitsam@samlr.com>
2014-09-17 21:51:37 +0100

    added canvas with basic addition of rocks also mute function

--- src/app.js
+++ src/app.js
@@ -11,9 +11,12 @@
     var oscillator = audioCtx.createOscillator();
     var gainNode = audioCtx.createGain();
     var initialGain = 0.05;
+    var WIDTH = window.innerWidth;
+    var HEIGHT = window.innerHeight;
+
+    var CurX, CurY;
 
     oscillator.connect(gainNode);
-    gainNode.connect(audioCtx.destination);
     
     gainNode.gain.value = initialGain;
 
@@ -23,12 +26,34 @@
 
     var mute = document.querySelector('#mute');
     mute.onclick = function() {
-        if (gainNode.gain.value) {
+        if (mute.getAttribute('data-muted') === "false") {
             gainNode.disconnect(audioCtx.destination);
+            mute.setAttribute('data-muted', 'true');
             mute.innerHTML = "unmute";
         } else {
             gainNode.connect(audioCtx.destination);
+            mute.setAttribute('data-muted', 'false');
             mute.innerHTML = "mute";
         }
     };
+
+    var canvas = document.querySelector('#garden');
+    canvas.width = WIDTH;
+    canvas.height = HEIGHT;
+
+    var canvasCtx = canvas.getContext('2d');
+
+    canvas.onclick = updatePage;
+
+    function updatePage (e) {
+        var bounding = canvas.getBoundingClientRect();
+        CurX = e.clientX - canvas.offsetLeft;
+        CurY = e.clientY - canvas.offsetTop;
+
+        console.log(CurX, CurY);
+
+        canvasCtx.fillStyle = "#FF0000";
+        canvasCtx.fillRect(CurX-25, CurY-25, 50, 50);
+    }
+
 }());
